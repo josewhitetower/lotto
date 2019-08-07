3a1c886cf8b9f6cf151f7f043971c50ffc534b63
Jose Torreblanca <josewhitetower@gmail.com>
2019-08-07 21:15:07 +0200

    first commit

---
+++ src/components/Lottery.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import LotteryBall from "./LotteryBall";
+export default class Lottery extends Component {
+  static defaultProps = {
+    title: "",
+    numBalls: 10,
+    maxNum: 100
+  };
+
+  state = {
+    nums: Array.from({ length: this.props.numBalls }, () =>
+      Math.floor(Math.random() * this.props.maxNum + 1)
+    )
+  };
+
+  generateBalls = () =>
+    Array.from({ length: this.props.numBalls }, () =>
+      Math.floor(Math.random() * this.props.maxNum + 1)
+    );
+
+  handleClick = () => {
+    this.setState({ nums: this.generateBalls() });
+  };
+
+  render() {
+    const balls = this.state.nums.map((num, index) => (
+      <LotteryBall key={index} num={num} />
+    ));
+
+    return (
+      <div className="Lottery">
+        {this.props.title && <h2>{this.props.title}</h2>}
+        <div className="Lottery-balls">{balls}</div>
+        <button className="Lottery-button" onClick={this.handleClick}>
+          Generate
+        </button>
+      </div>
+    );
+  }
+}
---
+++ src/components/LotteryBall.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LotteryBall({ num }) {
+  return <div className="LotteryBall">{num}</div>;
+}
